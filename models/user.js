47c127c7b942ef06ee9d6160ba75aa8fe71fa64c
aniketsingh1021 <144201758+aniketsingh1021@users.noreply.github.com>
2023-12-28 20:38:34 +0530

    changes

--- models/user.js
+++ models/user.js
@@ -10,13 +10,6 @@ const userSchema = new mongoose.Schema({
 	password: { type: String, required: true },
 });
 
-userSchema.methods.generateAuthToken = function () {
-	const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
-		expiresIn: "7d",
-	});
-	return token;
-};
-
 const User = mongoose.model("users", userSchema);
 
 const validate = (data1) => {
